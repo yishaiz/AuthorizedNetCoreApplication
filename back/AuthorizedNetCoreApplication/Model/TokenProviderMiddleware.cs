b11b181fd27873fe2e110d53b2017670b1b09ecd
yishaiz <yishai5555@gmail.com>
2017-05-17 09:11:33 +0300

    back

---
+++ back/AuthorizedNetCoreApplication/Model/TokenProviderMiddleware.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+using Microsoft.AspNetCore.Http.Internal;
+using System.IO;
+
+namespace AuthorizedNetCoreApplication.Model
+{
+    public class TokenProviderMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly TokenProviderOptions _options;
+
+        public TokenProviderMiddleware(
+            RequestDelegate next,
+            IOptions<TokenProviderOptions> options)
+        {
+            _next = next;
+            _options = options.Value;
+        }
+
+        public Task Invoke(HttpContext context)
+        {
+            // If the request path doesn't match, skip
+            if (!context.Request.Path.Equals(_options.Path, StringComparison.Ordinal))
+            {
+                return _next(context);
+            }
+
+            // Request must be POST //                      with Content-Type: application/x-www-form-urlencoded
+            if (!context.Request.Method.Equals("POST")) //  || !context.Request.HasFormContentType
+            {
+                context.Response.StatusCode = 400;
+                return context.Response.WriteAsync("Bad request.");
+            }
+
+            LoginUser user = GetLoginUserFromJson(context);
+
+            if (user == null)
+            {
+                context.Response.StatusCode = 400;
+                return context.Response.WriteAsync("Invalid username or password.");
+            }
+
+            bool isAuthenticatedUser =  IsAuthenticatedUser(user);
+            //bool isAuthenticatedUser = await IsAuthenticatedUser(user);
+
+
+            if (!isAuthenticatedUser)
+            {
+                context.Response.StatusCode = 400;
+                return context.Response.WriteAsync("Invalid username or password.");
+            }
+
+            //authenticated :)
+
+            return GenerateToken(context);
+        }
+
+
+        private LoginUser GetLoginUserFromJson(HttpContext context)
+        {
+            LoginUser user = null;
+
+            try
+            {
+                string jsonData = GetRequestJson(context);
+
+                user = JsonConvert.DeserializeObject<LoginUser>(jsonData);
+            }
+            catch (Exception ex)
+            {
+                //can log or do something...
+                //throw;
+            }
+
+            return user;
+        }
+
+        private string GetRequestJson(HttpContext context)
+        {
+            //context.Request.EnableRewind();
+
+            return new StreamReader(context.Request.Body).ReadToEnd();
+        }
+
+
+        //private Task<ClaimsIdentity> GetIdentity(string username, string password)
+        //{
+        //    if (username == "TEST" && password == "TEST123")
+        //    {
+        //        return Task.FromResult(new ClaimsIdentity(new System.Security.Principal.GenericIdentity(username, "Token"), new Claim[] { }));
+        //    }
+
+        //    // Credentials are invalid, or account doesn't exist
+        //    return Task.FromResult<ClaimsIdentity>(null);
+        //}
+
+        private   bool  IsAuthenticatedUser(LoginUser user)
+        {
+             bool isAuthenticatedUser = IsAuthenticatedUserFromConfig(user);
+
+            return isAuthenticatedUser;
+        }
+
+
+
+        private  bool  IsAuthenticatedUserFromConfig(LoginUser user)
+        {
+
+            //todo: get from config
+            return  user.Username == "TEST" && user.Password == "1234" ;
+            //return Task.FromResult(user.Username == "TEST" && user.Password == "1234");
+
+
+            throw new NotImplementedException();
+        }
+
+        private async Task GenerateToken(HttpContext context)
+        {
+            var username = context.Request.Form["username"];
+            var password = context.Request.Form["password"];
+
+            var identity = await GetIdentity(username, password);
+
+            if (identity == null)
+            {
+                context.Response.StatusCode = 400;
+                await context.Response.WriteAsync("Invalid username or password.");
+                return;
+            }
+
+            var now = DateTime.UtcNow;
+
+            // Specifically add the jti (random nonce), iat (issued timestamp), and sub (subject/user) claims.
+            // You can add other claims here, if you want:
+            var claims = new Claim[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, username),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                new Claim(JwtRegisteredClaimNames.Iat, now.ToString(), ClaimValueTypes.Integer64)
+            };
+
+            // Create the JWT and write it to a string
+            var jwt = new JwtSecurityToken(
+                issuer: _options.Issuer,
+                audience: _options.Audience,
+                claims: claims,
+                notBefore: now,
+                expires: now.Add(_options.Expiration),
+                signingCredentials: _options.SigningCredentials);
+
+            var encodedJwt = new JwtSecurityTokenHandler().WriteToken(jwt);
+
+            var response = new
+            {
+                access_token = encodedJwt,
+                expires_in = (int)_options.Expiration.TotalSeconds
+            };
+
+            // Serialize and return the response
+            context.Response.ContentType = "application/json";
+            await context.Response.WriteAsync(JsonConvert.SerializeObject(response, new JsonSerializerSettings { Formatting = Formatting.Indented }));
+        }
+
+        private Task<ClaimsIdentity> GetIdentity(string username, string password)
+        {
+            if (username == "TEST" && password == "TEST123")
+            {
+                return Task.FromResult(new ClaimsIdentity(new System.Security.Principal.GenericIdentity(username, "Token"), new Claim[] { }));
+            }
+
+            // Credentials are invalid, or account doesn't exist
+            return Task.FromResult<ClaimsIdentity>(null);
+        }
+
+    }
+}
---
+++ back/AuthorizedNetCoreApplication/Startup.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
+using AuthorizedNetCoreApplication.Model;
+using Microsoft.Extensions.Options;
+using System.Text;
+
+namespace AuthorizedNetCoreApplication
+{
+    public class Startup
+    {
+        // The secret key every token will be signed with.
+        // In production, you should store this securely in environment variables
+        // or a key management tool. Don't hardcode this into your application!
+        private static readonly string secretKey = "mysupersecret_secretkey!123";
+
+        public IConfigurationRoot Configuration { get; }
+
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+
+            if (env.IsDevelopment())
+            {
+                // This will push telemetry data through Application Insights pipeline faster, allowing you to view results immediately.
+                builder.AddApplicationInsightsSettings(developerMode: true);
+            }
+            Configuration = builder.Build();
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add framework services.
+            services.AddApplicationInsightsTelemetry(Configuration);
+
+            services.Configure<IdentityOptions>(options =>
+            {
+                options.Cookies.ApplicationCookie.AutomaticAuthenticate = true;
+                options.Cookies.ApplicationCookie.AutomaticChallenge = true;
+                options.Cookies.ApplicationCookie.AuthenticationScheme = "Cookie";
+            });
+
+            services.AddMvc();
+
+            services.AddSwaggerGen();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
+            app.UseApplicationInsightsRequestTelemetry();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseBrowserLink();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseApplicationInsightsExceptionTelemetry();
+
+            app.UseStaticFiles();
+
+            // ---------------------------------------------------------------------------------
+
+            //add the token middleware
+
+            // secretKey contains a secret passphrase only your server knows
+            //var secretKey = "mysupersecret_secretkey!123";
+            var signingKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(secretKey));
+
+            var jwtOptions = new TokenProviderOptions
+            {
+                Audience = "ExampleAudience",
+                Issuer = "ExampleIssuer",
+                SigningCredentials = new SigningCredentials(signingKey, SecurityAlgorithms.HmacSha256),
+            };
+
+            app.UseMiddleware<TokenProviderMiddleware>(Options.Create(jwtOptions));
+
+            var tokenValidationParameters = new TokenValidationParameters
+            {
+                // The signing key must match!
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = signingKey,
+
+                // Validate the JWT Issuer (iss) claim
+                ValidateIssuer = true,
+                ValidIssuer = "ExampleIssuer",
+
+                // Validate the JWT Audience (aud) claim
+                ValidateAudience = true,
+                ValidAudience = "ExampleAudience",
+
+                // Validate the token expiry
+                ValidateLifetime = true,
+
+                // If you want to allow a certain amount of clock drift, set that here:
+                ClockSkew = TimeSpan.Zero
+            };
+
+            app.UseJwtBearerAuthentication(new JwtBearerOptions
+            {
+                AutomaticAuthenticate = true,
+                AutomaticChallenge = true,
+                TokenValidationParameters = tokenValidationParameters,
+                AuthenticationScheme = "Bearer"
+            });
+
+            // ---------------------------------------------------------------------------------
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+
+            app.UseSwagger();
+            app.UseSwaggerUi();
+
+        }
+    }
+}
